dc3b91a4a099a21dd42525d2e9f37771e46a7ad1
YuliiaRez <qwerty959@gmail.com>
2022-08-21 21:24:58 +0300

    Get and sent MessageList

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,26 +1,7 @@
-// import React, { useContext } from "react";
-// import { Context } from "..";
-// import Button from "./Button";
-// import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-// import { Navigate } from "react-router-dom";
-// import { CHAT_ROUTE } from "../utils/consts";
-
-// function Login() {
-//   const { auth } = useContext(Context);
-
-//   return (
-//     <div>
-//       <div onClick={login}>Log in with Google</div>
-//     </div>
-//   );
-// }
-
-// export default Login;
-
 import React from "react";
 
 function Login() {
-  return <div>LOGIN</div>;
+  return <div>Please, register with Google Account</div>;
 }
 
 export default Login;
