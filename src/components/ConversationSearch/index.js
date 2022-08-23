7d9442732ca55bf935ee2c0ae6bd90280bc7bac6
YuliiaRez <qwerty959@gmail.com>
2022-08-23 10:09:21 +0300

    added searching in contacts

--- src/components/ConversationSearch/index.js
+++ src/components/ConversationSearch/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import './ConversationSearch.css';
+import React from "react";
+import "./ConversationSearch.css";
 
-export default function ConversationSearch() {
-    return (
-      <div className="conversation-search">
-        <input
-          type="search"
-          className="conversation-search-input"
-          placeholder="Search Messages"
-        />
-      </div>
-    );
+export default function ConversationSearch({ onChanging, search }) {
+  return (
+    <div className="conversation-search">
+      <input
+        type="search"
+        className="conversation-search-input"
+        placeholder="Search Contacts"
+        value={search}
+        onChange={(e) => onChanging(e)}
+        autoFocus
+        autoComplete="off"
+      />
+    </div>
+  );
 }
